ee7fb366e5c6aee01c42ac856d629a7247e980ac
Clifford Morin <cemorin21@gmail.com>
2023-07-26 13:44:32 -0700

    added new photos

--- src/components/Pictures/images.js
+++ src/components/Pictures/images.js
@@ -10,8 +10,96 @@ import guitar from "../../images/guitar.jpg";
 import saxes from "../../images/saxes.jpg";
 import dancers2 from "../../images/dancers2.jpg";
 import josh2 from "../../images/josh2.jpg";
+import contactPage from "../../images/contactPage.jpg";
+import band1 from "../../images/band1.jpeg";
+import band2 from "../../images/band2.jpeg";
+import band3 from "../../images/band3.jpeg";
+import band4 from "../../images/band4.jpeg";
+import band5 from "../../images/band5.jpg";
+import band6 from "../../images/band6.jpg";
+import band7 from "../../images/band7.jpg";
+import band8 from "../../images/band8.jpg";
+import band9 from "../../images/band9.PNG";
+import band10 from "../../images/band10.JPG";
+import band11 from "../../images/band11.jpg";
+import band12 from "../../images/band12.jpg";
+import band13 from "../../images/band13.jpg";
+import band14 from "../../images/band14.jpg";
+import band15 from "../../images/band15.jpg";
+import band16 from "../../images/band16.jpg";
 
 const images = [
+  {
+    original: band1,
+    thumbnail: band1,
+  },
+  {
+    original: band2,
+    thumbnail: band2,
+  },
+  {
+    original: band3,
+    thumbnail: band3,
+  },
+
+  {
+    original: contactPage,
+    thumbnail: contactPage,
+  },
+
+  {
+    original: band4,
+    thumbnail: band4,
+  },
+  {
+    original: band5,
+    thumbnail: band5,
+  },
+  {
+    original: band6,
+    thumbnail: band6,
+  },
+  {
+    original: band7,
+    thumbnail: band7,
+  },
+  {
+    original: band8,
+    thumbnail: band8,
+  },
+  {
+    original: band9,
+    thumbnail: band9,
+  },
+  {
+    original: band10,
+    thumbnail: band10,
+  },
+  {
+    original: band11,
+    thumbnail: band11,
+  },
+  {
+    original: band12,
+    thumbnail: band12,
+  },
+  {
+    original: band13,
+    thumbnail: band13,
+  },
+  {
+    original: band14,
+    thumbnail: band14,
+  },
+  {
+    original: band15,
+    thumbnail: band15,
+  },
+  {
+    original: band16,
+    thumbnail: band16,
+  },
+
   {
     original: saxes2,
     thumbnail: saxes2,
